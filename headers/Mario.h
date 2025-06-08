92a7fe3a17481353596febbb49b3ad1f687550fd
Do Manh Cuong <doomanhcuongg@gmail.com>
2025-06-08 20:41:58 +0700

    Demo character

---
+++ headers/Mario.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Character.h"
+#include <map>
+#include <raylib.h>
+
+class Mario : public Character {
+public :
+	Mario(Vector2 position);
+
+	void Update(float deltatime) override;
+	void LoadSource() override;
+	void Draw() override;
+
+};
---
+++ src/mario.cpp
@@ -0,0 +1,32 @@
+﻿#include "../headers/Mario.h"
+
+Mario::Mario(Vector2 position) {
+	this->position = position;
+	LoadSource();
+}
+
+void Mario::Update(float deltatime) {
+	animation[currentstate].Update(deltatime);
+}
+
+void Mario::LoadSource() {
+	texture = LoadTexture(".....");
+
+	Animation idle;
+
+	idle.frame = { {11, 18, 12, 16}, {43,17, 15, 16}, {76, 17, 15, 16} };// test thử
+	idle.currentframe = 0;
+	idle.durationtime = 0.3f;
+	idle.currenttime = 0;
+	animation[Actionstate::IDLE] = idle;
+
+
+}
+
+void Mario::Draw() {
+	Rectangle currentFrame = animation[currentstate].getcurrentframe();
+	float scale = 3.0f;
+	Rectangle dest = { position.x, position.y, currentFrame.x * scale, currentFrame.y * scale };
+	DrawTexturePro(texture, currentFrame, dest, { 0,0 }, 0, WHITE);
+
+}
