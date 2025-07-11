0d287b6832fc0cf9b9f7e15406954acc46b1cf92
anh10-1611 <triet.1611nguyen@gmail.com>
2025-07-11 19:40:17 +0700

    add music, add sounds and music to load

--- src/SoundManager.cpp
+++ src/SoundManager.cpp
@@ -21,14 +21,41 @@ void SoundManager::load() {
     sounds[SoundType::JUMP] = LoadSound("../assets/sfx/jump.wav");
     sounds[SoundType::COIN] = LoadSound("../assets/sfx/coin.wav");
     sounds[SoundType::DIE] = LoadSound("../assets/sfx/death.wav");
-    //....
+	sounds[SoundType::BRICK_BREAK] = LoadSound("../assets/sfx/bricks.wav");
+	sounds[SoundType::POWERUP] = LoadSound("../assets/sfx/powerup.wav");
+	sounds[SoundType::ONEUP] = LoadSound("../assets/sfx/1up.wav");
+	sounds[SoundType::BOWSERFALL] = LoadSound("../assets/sfx/bowserfall.wav");
+	sounds[SoundType::ITEMCOLLECT] = LoadSound("../assets/sfx/item.wav");
+	sounds[SoundType::HURRYUP] = LoadSound("../assets/sfx/hurryup.wav");
+	sounds[SoundType::FIREBALL] = LoadSound("../assets/sfx/fireball.wav");
+	sounds[SoundType::FLAGPOLE] = LoadSound("../assets/sfx/flagpole.wav");
+	sounds[SoundType::PIPEDOWN] = LoadSound("../assets/sfx/pipepowerdown.wav");
+    sounds[SoundType::STOMP_AND_SWIM] = LoadSound("../assets/sfx/stompswim.wav");
+	sounds[SoundType::KICKKILL] = LoadSound("../assets/sfx/kickkill.wav");
+	sounds[SoundType::BUMP] = LoadSound("../assets/sfx/bump.wav");
+	sounds[SoundType::JUMP_SMALL] = LoadSound("../assets/sfx/jumpsmall.wav");
+	sounds[SoundType::FIRE] = LoadSound("../assets/sfx/fire.wav");
+	sounds[SoundType::BILL_FIREWORK] = LoadSound("../assets/sfx/billfirework.wav");
+	sounds[SoundType::PAUSE] = LoadSound("../assets/sfx/pause.wav");
+	sounds[SoundType::GAMEOVER1] = LoadSound("../assets/sfx/gameover.wav");
+	sounds[SoundType::GAMEOVER2] = LoadSound("../assets/sfx/gameover2.wav");
+	sounds[SoundType::BEEP] = LoadSound("../assets/sfx/beep.wav");
+
 
     // Music
-    // musics[MusicType::OVERWORLD] = LoadMusicStream("../assets/music/overworld.mp3");
-    // musics[MusicType::UNDERGROUND] = LoadMusicStream("../assets/music/underground.mp3");
-    // musics[MusicType::BOSS] = LoadMusicStream("../assets/music/boss_theme.mp3");
-    // musics[MusicType::GAME_OVER] = LoadMusicStream("../assets/music/gameover.mp3");
-    //...
+     musics[MusicType::MAIN_THEME_OVERWORLD] = LoadMusicStream("../assets/music/DEFAULT_OVERWORLD_THEME.mp3");
+	 musics[MusicType::MAIN_THEME_UNDERGROUND] = LoadMusicStream("../assets/music/DEFAULT_UNDERGROUND_THEME.mp3");
+	 musics[MusicType::MAIN_THEME_CASTLE] = LoadMusicStream("../assets/music/DEFAULT_CASTLE_THEME.mp3");
+	 musics[MusicType::MAIN_THEME_UNDERWATER] = LoadMusicStream("../assets/music/DEFAULT_UNDERWATER_THEME.mp3");
+	 musics[MusicType::LEVEL_COMPLETE] = LoadMusicStream("../assets/music/LEVEL_COMPLETE.mp3");
+     musics[MusicType::WORLD_CLEAR] = LoadMusicStream("../assets/music/WORLD_CLEAR.mp3");
+     musics[MusicType::CASTLE_COMPLETE] = LoadMusicStream("../assets/music/CASTLE_COMPLETE.mp3");
+	 musics[MusicType::ENDING] = LoadMusicStream("../assets/music/ENDING.mp3");
+	 musics[MusicType::HURRY_UNDERGROUND] = LoadMusicStream("../assets/music/HURRY_UNDERGROUND.mp3");
+	 musics[MusicType::HURRY_OVERWORLD] = LoadMusicStream("../assets/music/HURRY_OVERWORLD.mp3");
+	 musics[MusicType::HURRY_CASTLE] = LoadMusicStream("../assets/music/HURRY_CASTLE.mp3");
+	 musics[MusicType::HURRY_UNDERWATER] = LoadMusicStream("../assets/music/HURRY_UNDERWATER.mp3");
+
 }
 
 void SoundManager::unload() {
